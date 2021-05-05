020c2dbb1093932b0b38e94dbfcd6eeb48d077b6
MAURICIO ALEJANDRO LOPEZ MONSERRATT <mau_ranger@hotmail.com>
2021-05-05 00:06:27 -0600

    Agregar Estructuras al proyecto y hacer referencias entre ellas

---
+++ ListaDobleEnlace/ListaDoble.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaDobleEnlace
+{
+    public class ListaDoble<T> : IEnumerable<T>
+    { 
+        public Nodo<T> inicio = new Nodo<T>();
+        private Nodo<T> fin = new Nodo<T>();
+        public int contador;
+
+
+        public ListaDoble()
+        {
+            inicio = null;
+            fin = null;
+            contador = 0;
+        }
+
+        ~ListaDoble() { }
+
+        bool ListaVacia()
+        {
+            return contador == 0;
+        }
+
+        public void Vaciar()
+        {
+            if(inicio != null)
+            {
+                inicio = null;
+                fin = null;
+                contador = 0;
+            }
+        }
+
+        public void InsertarInicio(T NuevoValor)
+        {
+            if(NuevoValor == null)
+            {
+                return;
+            }
+            Nodo<T> nuevoNodo = new Nodo<T>();
+            nuevoNodo.Valor = NuevoValor;
+
+            if (ListaVacia())
+            {
+                inicio = nuevoNodo;
+                fin = nuevoNodo;
+            }
+            else
+            {
+                nuevoNodo.Siguiente = inicio;
+                inicio.Anterior = nuevoNodo;
+                inicio = nuevoNodo;
+            }
+            contador++;
+            return;
+        }
+
+        public void InsertarFinal(T NuevoValor)
+        {
+            Nodo<T> nuevoNodo = new Nodo<T>();
+            nuevoNodo.Valor = NuevoValor;
+
+            if (ListaVacia())
+            {
+                inicio = nuevoNodo;
+                fin = nuevoNodo;
+            }
+            else
+            {
+                fin.Siguiente = nuevoNodo;
+                nuevoNodo.Anterior = fin; 
+                fin = nuevoNodo; 
+            }
+            contador++;
+            return;
+        }
+
+        public Nodo<T> ExtraerEnPosicion(int posicion)
+        {
+            Nodo<T> temporal = inicio;
+
+            if (!ListaVacia())
+            {
+                if ((contador == 1) || (posicion == 0))
+                {
+                    return ExtraerInicio();
+                }
+                else
+                {
+                    if (posicion >= contador - 1)
+                    {
+                        return ExtraerFinal();
+                    }
+                    else
+                    {
+                        Nodo<T> auxiliar = inicio;
+                        int pos = 1;
+
+                        while ((pos < posicion))
+                        {
+                            auxiliar = auxiliar.Siguiente;
+                            pos++;
+                        }
+                        auxiliar.Anterior.Siguiente = auxiliar.Siguiente;
+                        auxiliar.Siguiente.Anterior = auxiliar.Anterior;
+                        contador--;
+                    }
+                }
+            }
+            return temporal;
+        }
+        public T ObtenerValor(int posicion)
+        {
+            if (posicion >= 0 && posicion < contador)
+            {
+
+                Nodo<T> temporal = inicio;
+                int ubicacion = 0;
+
+                while (ubicacion < posicion)
+                {
+                    temporal = temporal.Siguiente;
+                    ubicacion++;
+                }
+                return temporal.Valor;
+            }
+            return default(T);
+        }
+
+        Nodo<T> ExtraerInicio()
+        {
+            Nodo<T> temporal = inicio;
+
+            if (!ListaVacia())
+            {
+                inicio = inicio.Siguiente;
+                inicio.Anterior = null;
+                if (contador == 1)
+                {
+                    fin = inicio;
+                }
+                contador--;
+            }
+            return temporal;
+        }
+
+        Nodo<T> ExtraerFinal()
+        {
+            Nodo<T> temporal = fin;
+
+            if (!ListaVacia())
+            {
+                if (contador == 1)
+                {
+                    fin = fin.Siguiente;
+                    inicio = fin;
+                }
+                else
+                {
+                    fin = fin.Anterior;
+                    fin.Siguiente = null;
+                }
+                contador--;
+            }
+            return temporal;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Nodo<T> nodo = inicio;
+            while (nodo != null)
+            {
+                yield return nodo.Valor;
+                nodo = nodo.Siguiente;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator(); 
+        }
+    }
+}
---
+++ THash/THash.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using ListaDobleEnlace;
+
+namespace THash
+{
+    public class THash<T> : ListaDoble<T> 
+    {
+        ListaDoble<T>[] HashTable = new ListaDoble<T>[10];
+        public ListaDoble<T> Lista0 = new ListaDoble<T>();
+        public ListaDoble<T> Lista1 = new ListaDoble<T>();
+        public ListaDoble<T> Lista2 = new ListaDoble<T>();
+        public ListaDoble<T> Lista3 = new ListaDoble<T>();
+        public ListaDoble<T> Lista4 = new ListaDoble<T>();
+        public ListaDoble<T> Lista5 = new ListaDoble<T>();
+        public ListaDoble<T> Lista6 = new ListaDoble<T>();
+        public ListaDoble<T> Lista7 = new ListaDoble<T>();
+        public ListaDoble<T> Lista8 = new ListaDoble<T>();
+        public ListaDoble<T> Lista9 = new ListaDoble<T>();
+
+        public THash()
+        {
+            HashTable[0] = Lista0;
+            HashTable[1] = Lista1;
+            HashTable[2] = Lista2;
+            HashTable[3] = Lista3;
+            HashTable[4] = Lista4;
+            HashTable[5] = Lista5;
+            HashTable[6] = Lista6;
+            HashTable[7] = Lista7;
+            HashTable[8] = Lista8;
+            HashTable[9] = Lista9;
+        }
+
+        public int Llave(string Titulo)
+        {
+            char[] array = new char[Titulo.Length];
+            char letra;
+            int posicion;
+            int codigo = 0;
+
+            for (int i = 0; i < Titulo.Length; i++)
+            {
+                array[i] = Titulo[i];
+                letra = array[i];
+                posicion = Convert.ToInt32(letra) * (i + 1);
+                codigo = posicion + codigo;
+                codigo = codigo % HashTable.Length;
+            }
+
+            return codigo;
+        }
+
+        public void Insertar(T valor, string Titulo)
+        {
+            int llave = Llave(Titulo);
+
+            switch (llave)
+            {
+                case 0:
+                    Lista0.InsertarInicio(valor);
+                    break;
+                case 1:
+                    Lista1.InsertarInicio(valor);
+                    break;
+                case 2:
+                    Lista2.InsertarInicio(valor);
+                    break;
+                case 3:
+                    Lista3.InsertarInicio(valor);
+                    break;
+                case 4:
+                    Lista4.InsertarInicio(valor);
+                    break;
+                case 5:
+                    Lista5.InsertarInicio(valor);
+                    break;
+                case 6:
+                    Lista6.InsertarInicio(valor);
+                    break;
+                case 7:
+                    Lista7.InsertarInicio(valor);
+                    break;
+                case 8:
+                    Lista8.InsertarInicio(valor);
+                    break;
+                case 9:
+                    Lista9.InsertarInicio(valor);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /* public T ObtenerValor(T titulo, int llave)
+        {
+            T valor;
+            bool encontrar = false;
+            int i = 0;
+            switch (llave)
+            {
+                case 0:
+                    while (encontrar == false)
+                    {
+                        valor = Lista0.ObtenerValor(i);
+                        if (valor.CompareTo(titulo) == 0)
+                            encontrar = true;
+                        else
+                            i++;
+                    }
+                    return valor;
+                    break;
+                case 1:
+                    Lista1.InsertarInicio(valor);
+                    break;
+                case 2:
+                    Lista2.InsertarInicio(valor);
+                    break;
+                case 3:
+                    Lista3.InsertarInicio(valor);
+                    break;
+                case 4:
+                    Lista4.InsertarInicio(valor);
+                    break;
+                case 5:
+                    Lista5.InsertarInicio(valor);
+                    break;
+                case 6:
+                    Lista6.InsertarInicio(valor);
+                    break;
+                case 7:
+                    Lista7.InsertarInicio(valor);
+                    break;
+                case 8:
+                    Lista8.InsertarInicio(valor);
+                    break;
+                case 9:
+                    Lista9.InsertarInicio(valor);
+                    break;
+                default:
+                    break;
+            }
+        }*/
+
+    }
+}
